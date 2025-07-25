87e4862f3eadd6b6f747c37f906cd91dba0fecfc
AKSHAYAKGCAS123 <24bcs204@kgcas.com>
2025-07-25 10:45:21 +0530

    Day4.js

---
+++ Day4.js
@@ -0,0 +1,52 @@
+// let details={
+//     name:"js",
+//     totalmark:100,
+//     subject:{
+//         dbms:100,
+//         java:100
+//     },
+//     sum:function(){
+//         let sum=this.subject.dbms+this.subject.java;
+//         return total;
+//     }
+
+// }
+// console.log(details.sum())
+// console.log(details.name);
+// console.log(details["totalmark"]);
+// console.log(details.subject.dbms);
+// console.log(details.subject["java"]);
+let arr=new Array(1,2,"three");
+console.log(arr[2]);
+
+let arr2=["js",true,false];
+//array destructuring
+const[name,b1,b2]=arr2;
+console.log(name)
+//filter-Returns a new array containing only the
+//elements that match a condition
+// let student=[
+//     {name:"akshaya", grade:"A",},
+//     {name:"joe",grade:"B"},
+//     {name:"ashmitha",grade:"A"}
+    
+    
+
+    
+
+// ]
+// let gradestudent=student.filter(students => students.grade==="A")
+// console.log(gradestudent);
+//slice
+
+let data=[1,2,3,4,5];
+let reasul=data.splice(2,4);
+console.log(result);
+
+
+
+
+
+
+
+
