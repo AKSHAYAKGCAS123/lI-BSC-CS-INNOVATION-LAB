7a8f2c6b565df9b512a0d9fe060e52d52af26063
Akshaya <24bcs204@kgcas.com>
2026-01-29 10:12:52 +0530

    program6.jsx

--- App.jsx
+++ App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [num1,setNum1]=useState("");
+  const [num2,setNum2]=useState("");
+  const a=Number(num1);
+  const b=Number(num2);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 style={{textDecoration:"underline"}}>Calculator</h1>
+      <input type="number"
+      placeholder='Enter the First Value'
+       onChange={(e)=>setNum1(e.target.value)}/>
+      <input type="number"
+      placeholder='Enter the Second Value'
+       onChange={(e)=>setNum2(e.target.value)}/>
+      <div style={{height:"200px",
+        width:"200px",
+        border:"1px solid black",
+        marginLeft:"300px",
+        marginTop:"20px",
+        backgroundColor:"lightblue"}}>
+          <h4>Results:</h4>
+          <p><strong>Addition:</strong>{a+b}</p>
+          <p><strong>Subtraction:</strong>{a-b}</p>
+          <p><strong>Multiplication:</strong>{a*b}</p>
+          <p><strong>Division:</strong>
+          {b==0 ?"can't divide by zero":a/b}
+          </p>
+          
+
+        </div>
+
+      
+    </div>
   )
 }
 
